6bfb7d05e1d918080bcfa1300287caa468e0b65b
Gil71-Gil72 <important.2@qq.com>
2026-06-25 17:10:39 +0800

    second commit

--- exam/src/components/HelloWorld.vue
+++ exam/src/components/HelloWorld.vue
@@ -1,5 +1,5 @@
+<h1>{{ msg }}</h1>
 <template>
-  <h1>{{ msg }}</h1>
   <button @click="count++">count is: {{ count }}</button>
   <p>Edit <code>components/HelloWorld.vue</code> to test hot module replacement.</p>
 </template>
